d0826a6da92b31277ff3272bbd8f9dc34582b74b
Amirul Azam <109798823+AmirulAzam7@users.noreply.github.com>
2022-07-27 00:58:59 +0600

    Create 1070.cpp

---
+++ 1070.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int n,i;
+    cin>>n;
+
+    for(i=n; i<=n+11; i++)
+    {
+        if(i%2!=0)
+        {
+            cout<<i<<endl;
+        }
+    }
+
+
+
+    return 0;
+}
